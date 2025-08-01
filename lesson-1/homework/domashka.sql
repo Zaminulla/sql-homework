b2f25b98f8dbeabff3cd6f5b860ddb7b41f8fced
Zaminulla <63052372+Zaminulla@users.noreply.github.com>
2025-08-01 18:55:16 +0500

    Create domashka.sql

---
+++ lesson-1/homework/domashka.sql
@@ -0,0 +1,8 @@
+Информация записана, передаеа или которая может быть измерена
+СOUNT(), sum(), AVG(), MIN(), MAX()
+WINDOWS AUTHENTICATION, SQL SERVER AUTHENTICATION
+CREATE DATABASE SchoolDB
+CREATE TABLE STUDENTS (STUDENTID INT PRIMARY KEY, NAME VARCHAR(50), AGE INT)
+SQL SERVER это инструмент для работы с базами данными, SSMS это приложение для работы с SQL сервером, а SQL это язык для работы с базами данными использзуется для создания, чтения и обновления
+DQL - SELECT, DML - INSERT UPDATE DELETE, DDL - CREATE ALTER DROP, DCL - GRANT REVOKE, TCL - ROLLBAK
+INSERT INTO STUDENTS VALUES(1, 'SHOX', 29), (2, 'BEKZAD', 22), (3, 'JOXA', 40)
