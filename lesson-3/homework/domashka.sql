c034ead1f5d5ecf67ef5920811e39d240a7b33a5
Zaminulla <63052372+Zaminulla@users.noreply.github.com>
2025-08-05 21:07:36 +0500

    Update domashka.sql

--- lesson-3/homework/domashka.sql
+++ lesson-3/homework/domashka.sql
@@ -8,3 +8,16 @@ ALTER TABLE Products add constraint productname_unique Unique (ProductName) -- �
 alter table products add CategoryId int
 create table Categories (CategoryId INT Primary key, CategoryName INT UNIQUE)
 IDENTITY это шаговое нумерация по заданному шагу
+bulk insert products from 'C:\Users\sh.zaminulla\Documents\lesson 3\student_data.txt' with(FIELDTERMINATOR=',', ROWTERMINATOR='\n', FIRSTROW=1)
+create table tovari (TovariID int) alter table tovari add constraint TovariId_kluch foreign key (TovariId) References Categories(CategoryId)
+Первичный ключ это уникальность сроки, а уникальный ключ это уникальность значенний
+alter table products add constraint Price check (price>0)
+alter table products add ZAPAS int NOT NULL default 0
+select price, isnull(price, 0) as price from products
+FOREIGN KEY подключается только одному внешнему ключу
+create table clients (clientsid int primary key, clients_name varchar(50) not null, age int not null, constraint chk_age check (age>=18))
+create table indetity_1 (id int identity(100,10))
+CREATE TABLE OrderDetails (OrderID INT NOT NULL, ProductID INT NOT NULL, Quantity INT NOT NULL, Price DECIMAL(10, 2) NOT NULL, CONSTRAINT PK_OrderDetails PRIMARY KEY (OrderID, ProductID))
+ISNULL — для простой подстановки одного значения (NULL → что-то) COALESCE — когда есть несколько возможных источников данных и нужно выбрать первое не-NULL значение
+create table sotrudniki(empID int primarykey, email int unique)
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Customers FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID) ON DELETE CASCADE ON UPDATE CASCADE
